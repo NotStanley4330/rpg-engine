3754773ffa5344c82a191117d707bb6dbd7db695
Michael Norris <michaelbrian.norris82@gmail.com>
2024-01-16 16:01:02 -0700

    Have display.c as a blank win32 window

--- test-stuff/display.c
+++ test-stuff/display.c
@@ -8,3 +8,84 @@
  *
  *
  *********************************************/
+
+#include <stdio.h>
+#include <windows.h>
+
+// Function prototypes
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int main() {
+    // Register window class
+    const char CLASS_NAME[] = "SimpleWindowClass";
+
+    WNDCLASS wc = {0};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+            0,                          // Optional window styles
+            CLASS_NAME,                 // Window class
+            "Example Display",            // Window text
+            WS_OVERLAPPEDWINDOW,        // Window style
+
+            // Size and position
+            CW_USEDEFAULT, CW_USEDEFAULT, 1000, 800,
+
+            NULL,       // Parent window
+            NULL,       // Menu
+            GetModuleHandle(NULL), // Instance handle
+            NULL        // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    // Display the window
+    ShowWindow(hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(hwnd);
+
+    // Message loop
+    //This is where we handle any event messages sent by the OS (ie Mouse Clicks)
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+// Window procedure
+//This is how we handle translate messages
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW+1));
+            EndPaint(hwnd, &ps);
+        }
+            return 0;
+        case WM_CLOSE:
+            if (MessageBox(hwnd, "Really quit?", "Quit dialog", MB_OKCANCEL) == IDOK)
+            {
+                DestroyWindow(hwnd);
+            }
+            // Else: User canceled. Do nothing.
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
